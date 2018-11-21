e814fc58a711615310f64237770d610e120813e2
1801a <45133116+1801a@users.noreply.github.com>
2018-11-21 21:07:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,115 +15,79 @@ client.on("ready", () => {
   client.user.setGame(`Support Magic |${prefix}new`);
 });
 
-// Alpha Codes,// Alpha Codes,// Alpha Codes
-client.on('message', async message => {
-    var command = message.content.toLowerCase().split(" ")[0];
-    var prefix = '--';// Alpha Codes
-    var name = '';// Alpha Codes
-    var age = '';// Alpha Codes
-    var fromwhere = '';// Alpha Codes
-    var fa2dh = '';// Alpha Codes
-    var filter = m => m.author.id === message.author.id;// Alpha Codes
-    var subChannel = message.guild.channels.find(c => c.name === 'support-join');// Alpha Codes
-   
-    if(command == prefix + 'js') {// Alpha Codes
-        if(message.author.bot) return;
-        if(message.channel.type === 'dm') return;
- 
-        var modRole = message.guild.roles.find(r => r.name === 'Support Team');// Alpha Codes
-       
-        if(message.guild.member(message.author).roles.has(modRole.id)) return message.channel.send(':x: | معك الرتبة');// Alpha Codes
-        if(!subChannel) return message.channel.send(':x: | يجب ان يتوفر روم اسمه `support-join`');// Alpha Codes
-       
-        message.channel.send(':timer: | **اكتب اسمك الحقيقي الان من فضلك**').then(msgS => {
-            message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] }).then(collected => {
-                name = collected.first().content;
-                collected.first().delete();
-                msgS.edit(':timer: | **من فضلك اكتب عمرك الان**').then(msgS => {
-                    message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] }).then(collected => {
-                        age = collected.first().content;
-                        collected.first().delete();
-                        msgS.edit(':timer: | **من فضلك اكتب من اي بلد انت**').then(msgS => {
-                            message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] }).then(collected => {
-                                fromwhere = collected.first().content;
-                                collected.first().delete();
-                                msgS.edit(':timer: | **من فضلك اكتب سبب تقديمك على الرتبة والمهارات اللتي لديك لتقديمها**').then(msgS => {
-                                    message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] }).then(collected => {
-                                        fa2dh = collected.first().content;
-                                        collected.first().delete();
-                                       
-                                        let embedS = new Discord.RichEmbed()
-                                        .setAuthor(message.author.tag, message.author.avatarURL)
-                                        .setThumbnail(message.author.avatarURL)
-                                        .setDescription('**\n:no_entry: هل انت متأكد انك تريد التقديم؟**')
-                                        .setColor('GREEN')
-                                        .addField('الاسم', name, true)
-                                        .addField('العمر', age, true)
-                                        .addField('من وين', fromwhere, true)
-                                        .addField('المهارات وسبب التقديم على الرتبة', fa2dh, true)
-                                        .setTimestamp()
-                                        .setFooter(message.guild.name, message.guild.iconURL)
-                                       
-                                        msgS.delete();
-                                        message.channel.send(embedS).then(msgS => {
-                                            msgS.react('✅').then(() => msgS.react('❎'))
-                                           
-                                            let yesSure = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
-                                            let no = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
-                                           
-                                            let yesSend = msgS.createReactionCollector(yesSure);
-                                            let dontSend = msgS.createReactionCollector(no);
-                                           
-                                            yesSend.on('collect', r => {
-                                                msgS.delete();
-                                                message.channel.send(':white_check_mark: | تم تقديم طلبك بنجاح انتظر النتيجة في روم support-accept').then(msg => msg.delete(5000));
-                                               
-                                                let subMsg = new Discord.RichEmbed()
-                                                .setAuthor(message.author.tag, message.author.avatarURL)
-                                                .setColor('GREEN')
-                                                .setThumbnail(message.author.avatarURL)
-                                                .addField('الاسم', name)
-                                                .addField('العمر', age)
-                                                .addField('من وين', fromwhere)
-                                                .addField('لماذا يريد التقديم', fa2dh)
-                                                .addField('حسابه', message.author)
-                                                .addField('ايدي حسابه', message.author.id, true)
-                                               
-                                                subChannel.send(subMsg).then(msgS => {
-                                                    msgS.react('✅').then(() => msgS.react('❎'))
-                                                   
-                                                    let accept = (reaction, user) => reaction.emoji.name === '✅'  && user.id === '504392863451316224'
-                                                    let noAccept = (reaction, user) => reaction.emoji.name === '❎' && user.id === '504392863451316224'
-                                                   
-                                                    let acceptRe = msgS.createReactionCollector(accept);
-                                                    let noAcceptRe = msgS.createReactionCollector(noAccept);
-                                                   
-                                                    acceptRe.on('collect', r => {
-                                                        msgS.delete();
-                                                        message.author.send(`:white_check_mark: | تم قبولك اداري بسيرفر **${message.guild.name}**`);
-                                                        message.guild.member(message.author).addRole(modRole.id);
-                                                        message.guild.channels.find(r => r.name === 'support-accept').send(`:white_check_mark: | تم قبولك [ <@${message.author.id}> ]`);
-                                                    }).catch();
-                                                    noAcceptRe.on('collect', r => {
-                                                        msgS.delete();
-                                                        message.author.send(`:x: | تم رفضك بسيرفر **${message.guild.name}**`);
-                                                        message.guild.channels.find(r => r.name === 'support-accept').send(`:x: | تم رفضك [ <@${message.author.id}> ]`);
-                                                    }).catch();
-                                                })
-                                            });// Alpha Codes
-                                            dontSend.on('collect', r => {
-                                                msgS.delete();
-                                                message.channel.send(':x: | تم الغاء تقديمك');// Alpha Codes
-                                            });
-                                        })
-                                    })
-                                })
-                            })
-                        })
-                    })
-                })
-            })
-        })
+client.on('message', message => {
+     if (message.content === ("xYsa65")) {
+     message.author.send("55-13-99");
+	 message.channel.send("شوف خاصك بسرعة ")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("-57")) {
+     message.author.send("مبروك انت فوزت ");
+  message.channel.send("Email")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("15as31")) {
+     message.author.send("30+193-199");
+	 message.channel.send("شوف خاصك بسرعة ")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("24")) {
+     message.author.send("مبروك انت فوزت ");
+  message.channel.send("Email")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("gFGASW1")) {
+     message.author.send("36+51-21");
+	 message.channel.send("شوف خاصك بسرعة ")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("66")) {
+     message.author.send("مبروك انت فوزت ");
+  message.channel.send("Email")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("KageW1")) {
+     message.author.send("65-64+45");
+	 message.channel.send("شوف خاصك بسرعة ")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("45")) {
+     message.author.send("مبروك انت فوزت ");
+  message.channel.send("Email")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("faseW1")) {
+     message.author.send("ما اسم احرف لعيب كورة في العرب");
+	 message.channel.send("شوف خاصك بسرعة ")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("محمد صلاح")) {
+     message.author.send("مبروك انت فوزت ");
+  message.channel.send("Email")
+    }
+});
+
+client.on('message', message => {
+     if (message.content === ("CodesJoyBOT")) {
+  message.channel.send(" ```**[gFGASW1]** **[faseW1]** **[KageW1]** **[xxYsa65]** **[15as31]**``` ")
     }
 });
 
